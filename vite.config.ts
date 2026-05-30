470124b8f7b5ec5e2c02b8363989693ab25b753c
Steven <steventan6002@gmail.com>
2026-05-30 22:13:26 +0700

    fix: adjusted vite.config.ts for ocr too

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,8 @@ export default defineConfig({
     rolldownOptions: {
       input: {
         popup: "src/popup/popup.ts",
+        background: "src/background/background.ts",
+        offscreen: "src/offscreen/offscreen.ts",
       },
       output: {
         entryFileNames: "[name].js",
